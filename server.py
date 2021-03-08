75a80532a1106f4893c4604fce15fc3ca7228155
git-avinash <mail.avinashsah@gmail.com>
2021-03-09 02:27:31 +0530

    release 01

--- server.py
+++ server.py
@@ -1,13 +1,22 @@
 import socketio
+import json
 from helpers import processTextifyRequest
 
 sio = socketio.AsyncServer(async_mode="asgi", cors_allowed_origins="*")
 app = socketio.ASGIApp(sio)
 
+online = 0
+like = 0
+
 
 @sio.event
 async def connect(sid, environ):
     print(f"[CONNECTED] {sid}")
+    global online
+    online += 1
+
+    await sio.emit("serverStats", json.dumps({"online": online, "like": like}), to=sid)
+    await sio.emit("updateOnline", online)
 
 
 @sio.event
@@ -16,5 +25,16 @@ async def getText(sid, data):
 
 
 @sio.event
+async def doLike(sid):
+    global like
+    like += 1
+    await sio.emit("updateLike", like)
+
+
+@sio.event
 async def disconnect(sid):
     print(f"[DISCONNECTED] {sid}")
+    global online
+
+    online -= 1
+    await sio.emit("updateOnline", online)
