1c74d743feefa0899ea540c7aea0c0f0bc0c7a05
git-avinash <mail.avinashsah@gmail.com>
2021-03-09 00:24:12 +0530

    bug fixes

--- helpers.py
+++ helpers.py
@@ -1,17 +1,22 @@
-import cv2
+from concurrent.futures import ProcessPoolExecutor
+import asyncio
 import base64
-import numpy as np
-from PIL import Image
 import pytesseract
+from PIL import Image
+import numpy as np
+import cv2
+
 
 pytesseract.pytesseract.tesseract_cmd = r"./Tesseract-OCR/tesseract.exe"
 
+
 def _decodeBase64String(base64String):
-    image = base64.b64decode(base64String); 
-    npImage = np.frombuffer(image, dtype=np.uint8); 
-    source = cv2.imdecode(npImage, 1) 
+    image = base64.b64decode(base64String)
+    npImage = np.frombuffer(image, dtype=np.uint8)
+    source = cv2.imdecode(npImage, 1)
 
     return source
+
 
 def _manipulateImage(npArray):
     grayImage = cv2.cvtColor(npArray, cv2.COLOR_BGR2GRAY)
@@ -19,22 +24,34 @@ def _manipulateImage(npArray):
     # cv2.imwrite("out.png", grayImage) # Test ManipulateImage by Saving
     return grayImage
 
+
 def _getTextFromOCR(npArray):
     with Image.fromarray(npArray) as image:
         text = pytesseract.image_to_string(image, lang="eng")
-    
+
     correctedText = "".join(text[:-2])
 
     return correctedText
 
+
 def getTextFromBase64(base64String):
-    decodeded = _decodeBase64String(base64String)
+    base64_string = "".join(base64String.split(",")[1]).encode("utf-8")
+
+    decodeded = _decodeBase64String(base64_string)
     manipulated = _manipulateImage(decodeded)
     textified = _getTextFromOCR(manipulated)
 
     return textified
 
-# with open("in.png", "rb") as image_file:
-#     encoded_string = base64.b64encode(image_file.read())
 
-# print(getTextFromBase64(encoded_string))
+async def processTextifyRequest(sio, sid, data):
+    loop = asyncio.get_running_loop()
+
+    print(f"[REQUEST RECEIVED] {sid}")
+
+    with ProcessPoolExecutor() as pool:
+        result = await loop.run_in_executor(pool, getTextFromBase64, data)
+
+        print(f"[PROCESSED] {sid}")
+
+        await sio.emit("text", result, to=sid)
--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import socketio
-from helpers import getTextFromBase64
+from helpers import processTextifyRequest
 
 sio = socketio.AsyncServer(async_mode="asgi", cors_allowed_origins="*")
 app = socketio.ASGIApp(sio)
@@ -7,18 +7,14 @@ app = socketio.ASGIApp(sio)
 
 @sio.event
 async def connect(sid, environ):
-    print(sid, "CONNECTED")
+    print(f"[CONNECTED] {sid}")
 
 
 @sio.event
 async def getText(sid, data):
-    print(">>>>>>>>>>>>>TRIGGERED<<<<<<<<<<<<<<<<<<<")
-    b64_string = "".join(data.split(",")[1]).encode("utf-8")
-    text = getTextFromBase64(b64_string)
-    print(text)
-    await sio.emit("text", text, to=sid)
+    await processTextifyRequest(sio=sio, sid=sid, data=data)
 
 
 @sio.event
 async def disconnect(sid):
-    print(sid, "DISCONNECTED")
+    print(f"[DISCONNECTED] {sid}")
